cf6384a449285c8fc1c0a4f8be3beb285d5766a1
PMPhuc <phucphan2302@gmail.com>
2023-12-08 07:02:17 +0700

    fix api

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,64 +1,63 @@
 const userPromises = require('../promises/userPromises.js');
 
-const login = async(req, res) => {
-    const { userTel, userPass } = req.body;
-    // Kiểm tra userTel có rỗng không
-    if (!userTel) {
-      return res.status(400).json({ error: 'Số điện thoại không được bỏ trống.' });
-    }
-    // Kiểm tra userTel có khoảng trắng hay có kí tự đb không
-    else if (isNaN(userTel) || userTel.includes(' ')) {
-      return res.status(400).json({ error: 'Số điện thoại không hợp lệ.' });
-    }
-    try {
-        const result = await userPromises.callAuthenticateUser(userTel, userPass);
-        return res.status(200).json({ result: result.message });
-    } catch (error) {
-        return res.status(500).json({ error: 'Đã xảy ra lỗi khi xác thực người dùng.' });
-    }
+const login = async (req, res) => {
+  const { userTel, userPass } = req.body;
+
+  // Check if userTel is empty
+  if (!userTel) {
+    return res.status(400).json({ error: 'Số điện thoại không được bỏ trống.' });
+  } else if (isNaN(userTel) || userTel.includes(' ')) {
+    // Check if userTel is invalid
+    return res.status(400).json({ error: 'Số điện thoại không hợp lệ.' });
+  }
+
+  try {
+    const result = await userPromises.callAuthenticateUser(userTel, userPass);
+    return res.status(200).json({ result: result.message });
+  } catch (error) {
+    return res.status(500).json({ error: 'Đã xảy ra lỗi khi xác thực người dùng.' });
+  }
 };
 
-const userInfor = async(req, res) => {
-    const userTel = req.params.user_mail;
-    // console.log(userTel);
-    try {
-      const user = await userPromises.callGetUser(userTel);
-      if (user.length === 0) {
-        return res.status(404).json({ error: 'Không tìm thấy thông tin người dùng.' });
-      }
-      return res.json(user);
-    } catch (error) {
-      // console.error(error);
-      return res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy thông tin người dùng.' });
+const userInfor = async (req, res) => {
+  const userAcc = req.params.user_acc;
+  console.log(userAcc);
+  try {
+    const user = await userPromises.callGetUser(userAcc);
+    if (user.length === 0) {
+      return res.status(404).json({ error: 'Không tìm thấy thông tin người dùng.' });
     }
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).json({ error: 'Đã xảy ra lỗi khi lấy thông tin người dùng.' });
+  }
 };
 
-const userAdd = async(req, res) => {
-    const { userTel, userPass, userName, userAva } = req.body;
+const userAdd = async (req, res) => {
+  const { userMail, userTel, userPass, userName, userAva, userAcc } = req.body;
 
-    try {
-        const result = await userPromises.callAddUser(userTel, userPass, userName, userAva);
-        return res.status(200).json({ message: result.message });
-    } catch (error) {
-        return res.status(500).json({ error: 'Đã xảy ra lỗi khi thêm user.' });
-    }
+  try {
+    const result = await userPromises.callAddUser(userMail, userTel, userPass, userName, userAva, userAcc);
+    return res.status(200).json({ message: result.message });
+  } catch (error) {
+    return res.status(500).json({ error: 'Đã xảy ra lỗi khi thêm user.' });
+  }
 };
 
-const userInforUpdate = async(req, res) => {
-    const { userTel, userPass, userName, userAva, userVIP } = req.body;
+const userInforUpdate = async (req, res) => {
+  const { userMail, userTel, userPass, userName, userAva, userAcc } = req.body;
 
-    try {
-        const result = await userPromises.callUpdateUser(userTel, userPass, userName, userAva, userVIP);
-        return res.status(200).json({ message: result.message });
-    } catch (error) {
-        return res.status(500).json({ error: 'Đã xảy ra lỗi khi cập nhật thông tin người dùng.' });
-    }
+  try {
+    const result = await userPromises.callUpdateUser(userMail, userTel, userPass, userName, userAva, userAcc);
+    return res.status(200).json({ message: result.message });
+  } catch (error) {
+    return res.status(500).json({ error: 'Đã xảy ra lỗi khi cập nhật thông tin người dùng.' });
+  }
 };
-
 
 module.exports = {
-    login,
-    userInfor,
-    userAdd,
-    userInforUpdate,
+  login,
+  userInfor,
+  userAdd,
+  userInforUpdate,
 };
