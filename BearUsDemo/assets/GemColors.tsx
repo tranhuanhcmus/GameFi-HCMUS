c985e9d4cfae90a00e6e7477bd1395259bcc6294
PMPhuc <phucphan2302@gmail.com>
2023-12-20 19:56:24 +0700

    Demo gem game

---
+++ BearUsDemo/assets/GemColors.tsx
@@ -0,0 +1,9 @@
+const GemColors = [
+  'https://i.ibb.co/VQNS1tc/1.png',
+  'https://i.ibb.co/4W5Mssn/2.png',
+  'https://i.ibb.co/0sgQXRQ/3.png',
+  'https://i.ibb.co/tXCC24Q/5.png',
+  'https://i.ibb.co/wR9yf19/6.png',
+];
+
+export default GemColors;
