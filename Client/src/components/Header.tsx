90676f0f5a4a9373912a9985a25eee994bce5e92
tranhuanhcmus <tranhuanbs02@gmail.com>
2024-03-01 13:46:18 +0700

    add button

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -5,6 +5,7 @@ import SVGProfile from "../../assets/SVGProfile.svg";
 import Ellipse from "../../assets/Ellipse.svg";
 import Polygon from "../../assets/Polygon.svg";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { W3mAccountButton } from "@web3modal/wagmi-react-native";
 
 interface HeaderProps {
   name: string;
@@ -14,15 +15,7 @@ const Header: React.FC<HeaderProps> = ({ name }) => {
   return (
     <View className="relative flex h-full w-screen flex-row items-center justify-center ">
       <View className="flex h-[80%] w-[40%] flex-row items-center justify-between rounded-[20px] bg-gray-200 px-4 py-2">
-        <View className="flex flex-row items-center gap-2">
-          <Ellipse></Ellipse>
-          <Text className="text-[18px]  font-bold">10</Text>
-        </View>
-
-        <View className="flex flex-row items-center gap-2">
-          <Polygon></Polygon>
-          <Text className="text-[18px]  font-bold">100</Text>
-        </View>
+        <W3mAccountButton balance="show" />
       </View>
       <View className="absolute left-2  ">
         <SVGSetting height={30} width={30} />
