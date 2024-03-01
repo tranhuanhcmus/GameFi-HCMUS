11961a6bd21116bb570e699b83a39372f6deeb62
tranhuanhcmus <tranhuanbs02@gmail.com>
2024-03-01 23:21:50 +0700

    changeDB

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-  connectionString: 'postgresql://postgres:K20_GameFi_DATN_HCMUS@db.gawnnxzgrxkdmhmzhssw.supabase.co:5432/postgres',
+  connectionString: 'postgres://postgres.gawnnxzgrxkdmhmzhssw:K20_GameFi_DATN_HCMUS@aws-0-us-west-1.pooler.supabase.com:5432/postgres',
 });
 
 module.exports = db;
