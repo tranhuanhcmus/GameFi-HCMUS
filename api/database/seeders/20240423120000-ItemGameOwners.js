b8cc38d7dc19ad0224a1f1fbfd50e254984500e5
phucphanminh <phucphan2302@gmail.com>
2024-05-15 12:24:06 +0700

    Add api for getting owner app and game items

--- api/database/seeders/20240423120000-ItemGameOwners.js
+++ api/database/seeders/20240423120000-ItemGameOwners.js
@@ -15,21 +15,21 @@ module.exports = {
 
      await queryInterface.bulkInsert('ItemGameOwners',[
       {
-        "id": "b3a05f52-6131-4dfc-ac6f-323231c79d25",
+        "id": "baebb924-5be9-4a6a-81d5-80fcc5c5ba48",
         "owner": "0xFe25C8BB510D24ab8B3237294D1A8fCC93241454",
         "quantity": 100,
         "createdAt": "2024-04-20T08:52:28.105Z",
         "updatedAt": "2024-04-20T08:52:28.105Z"
       },
       {
-        "id": "b3a05f52-6131-4dfc-ac6f-323231c79d26",
+        "id": "b86e38fe-c9e2-4437-8f7e-08455b3e4c8e",
         "owner": "0xFe25C8BB510D24ab8B3237294D1A8fCC93241454",
         "quantity": 100,
         "createdAt": "2024-04-20T08:52:28.105Z",
         "updatedAt": "2024-04-20T08:52:28.105Z"
       },
       {
-        "id": "b3a05f52-6131-4dfc-ac6f-323231c79d27",
+        "id": "a8affeaf-f7ba-4374-b42a-32422c521b0c",
         "owner": "0xFe25C8BB510D24ab8B3237294D1A8fCC93241454",
         "quantity": 100,
         "createdAt": "2024-04-20T08:52:28.105Z",
