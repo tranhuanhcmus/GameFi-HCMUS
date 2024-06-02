58f8c6ccf0ccd59f69c541aaeac7bf18b8933196
luckaruto <lucmten@gmail.com>
2024-06-02 10:01:17 +0700

    add some change about ui and assets for game

--- Client/src/Screens/HangManGame/Header.tsx
+++ Client/src/Screens/HangManGame/Header.tsx
@@ -143,19 +143,25 @@ const User: React.FC<props> = ({ hp }) => {
       </View>
 
       <View style={styles.playerPet}>
-        <SpriteSheet
-          ref={mummyRef1}
-          source={require("../../../assets/spritesheet_6.png")}
-          columns={19}
-          rows={1}
-          height={ConstantsResponsive.YR * 150}
-          width={ConstantsResponsive.XR * 150}
-          animations={{
-            walk: [
-              0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-            ],
-          }}
-        />
+        <View style={{ transform: [{ scaleX: -1 }] }}>
+          <SpriteSheet
+            ref={mummyRef1}
+            source={require("../../../assets/spritesSheet_18.png")}
+            columns={60}
+            rows={1}
+            height={ConstantsResponsive.YR * 250}
+            width={ConstantsResponsive.XR * 250}
+            animations={{
+              walk: [
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+                34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+              ],
+            }}
+          />
+        </View>
+
         {/* Your Animated Text for displaying damage */}
         <Animated.Text style={[styles.damageText, floatingTextStyle]}>
           -10
@@ -291,14 +297,17 @@ const Component: React.FC<props> = ({ hp }) => {
       <View style={styles.playerPet}>
         <SpriteSheet
           ref={mummyRef1}
-          source={require("../../../assets/spritesheet_7.png")}
-          columns={19}
+          source={require("../../../assets/spritesSheet_18.png")}
+          columns={60}
           rows={1}
-          height={ConstantsResponsive.YR * 150}
-          width={ConstantsResponsive.XR * 150}
+          height={ConstantsResponsive.YR * 250}
+          width={ConstantsResponsive.XR * 250}
           animations={{
             walk: [
               0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+              19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+              35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+              51, 52, 53, 54, 55, 56, 57, 58, 59,
             ],
           }}
         />
