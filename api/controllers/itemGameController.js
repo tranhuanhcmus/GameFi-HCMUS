a6941b34abb0d037b7dde00f179ea89b9fe489ba
tranhuanhcmus <tranhuanbs02@gmail.com>
2024-06-20 01:24:17 +0700

    add item game filter

--- api/controllers/itemGameController.js
+++ api/controllers/itemGameController.js
@@ -1,8 +1,58 @@
-const { STATUS_CODES } = require("../constants")
+const { Sequelize, Op } = require("sequelize");
+const { STATUS_CODES, ITEM_CATEGORY } = require("../constants")
 const models = require("../database/models")
 const getAll = async(req, res, next) => {
     try {
-        const list = await models.ItemGame.findAll()
+
+        let category_filter = {};
+        let quality_filter = {};
+
+        if (req.query && req.query.category) {
+            switch (req.query.category) {
+                case 'shop':
+                    category_filter = { category: {
+                            [Op.in]: ITEM_CATEGORY } };
+                    break;
+                case 'boost':
+                    category_filter = { category: 'boost' };
+                    break;
+                case 'food':
+                    category_filter = { category: 'food' };
+                    break;
+                case 'treasure':
+                    category_filter = { category: 'treasure' };
+                    break;
+                case 'energy':
+                    category_filter = { category: 'energy' };
+                    break;
+                default:
+                    // Handle unknown category, if needed
+                    break;
+            }
+        }
+        if (req.query && req.query.quality) {
+            switch (req.query.quality) {
+                case 'normal':
+                    quality_filter = { quality: 'normal' };
+                    break;
+                case 'rare':
+                    quality_filter = { quality: 'rare' };
+                    break;
+                case 'super rare':
+                    quality_filter = { quality: 'super rare' };
+                    break;
+                
+                default:
+                    // Handle unknown quality, if needed
+                    break;
+            }
+        }
+
+        let checkFilter = req.query ? true : false
+
+        const whereClause = { where: {...category_filter,...quality_filter } };
+
+        const list = await models.ItemGame.findAll(whereClause)
 
         return res.sendResponse(list, "Get All Success", STATUS_CODES.OK)
     } catch (error) {
@@ -45,7 +95,7 @@ const deleteById = async(req, res, next) => {
 
 const add = async(req, res, next) => {
     try {
-        let formData = { ...req.body, image: '/uploads/'+req.body?.imageName};
+        let formData = {...req.body, image: '/uploads/' + req.body.imageName };
         const newRow = await models.ItemGame.create(formData)
 
         return res.sendResponse(newRow, `Add success`, STATUS_CODES.OK)
@@ -76,6 +126,10 @@ const updateById = async(req, res, next) => {
     }
 }
 
-module.exports={
-	getAll,getById,add,deleteById,updateById
+module.exports = {
+    getAll,
+    getById,
+    add,
+    deleteById,
+    updateById
 }
