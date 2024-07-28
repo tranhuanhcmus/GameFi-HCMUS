2a9b58876bfc9f3173fc4b06d1d0d95baaec34c1
tranhuanhcmus <tranhuanbs02@gmail.com>
2024-07-28 17:19:44 +0700

    update DB

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { ContractController } = require('../controllers/ContractController');
 
 
 // /users
 router.get('/energy/:owner',userController.getEnergyOwner)
 router.get('/energyNFT/:tokenId',userController.getEnergyNFT)
 router.post('/energy/:owner',userController.updateEnergyOwner)
-router.post('/contract/updateDB',ContractController.updateDB)
+router.post('/contract/updateDB',userController.updateDB)
 
 module.exports = router;
