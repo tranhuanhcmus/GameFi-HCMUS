b68b8532e03eebc1e56c37a06ed3847ca04b8593
tranhuanhcmus <tranhuanbs02@gmail.com>
2024-08-05 23:59:24 +0700

    change url server

--- ShowCaseWeb/src/apis/index.jsx
+++ ShowCaseWeb/src/apis/index.jsx
@@ -16,6 +16,6 @@ export const api = axios.create({
   // baseURL: "https://gamefi-hcmus.onrender.com",
   
 //   baseURL: "https://gamefi-hcmus.onrender.com",
-  baseURL: "https://gamefi-hcmus.onrender.com",
+  baseURL: "https://gamefi-hcmus-aq3q.onrender.com",
   timeout: 30000,
 });
